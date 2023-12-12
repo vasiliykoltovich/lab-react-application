ba54d2bb716722b81f7d576a7180ce0f4b148a6a
Vasil.Koltovich <vasil.koltovich.ext@vc-trade.de>
2023-12-12 14:44:59 +0100

    lab react application

---
+++ api/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const connectDb = require('./src/connection');
+const User = require('./src/User.model');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.API_PORT;
+console.log("Connection port of API:", PORT)
+app.use(cors());
+
+app.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+app.get('/user-create', async (req, res) => {
+    const user = new User({ username: 'userTest' });
+    await user.save().then(() => console.log('User created'));
+    res.send('User created \n');
+});
+
+app.listen(PORT, function() {
+    console.log(`Listening on ${PORT}`);
+
+    connectDb().then(() => {
+        console.log('MongoDb connected');
+    });
+});
---
+++ api/src/connection.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const connection = process.env.MONGODB_URL
+console.log("Connection url to mongo:", connection)
+const connectDb = () => {
+    return mongoose.connect(connection);
+};
+module.exports = connectDb;
